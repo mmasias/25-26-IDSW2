c1019539379dbfb9bce15de8594bf88f3066e771
miguelrivasmorantes <miguelrivasmorantes@gmail.com>
2026-02-23 00:33:56 +0100

    feature/add #2 iteration files

---
+++ entregas/rivasMiguel/src/v2/Medio.java
@@ -0,0 +1,11 @@
+public class Medio {
+    private Lenguaje lenguaje;
+
+    public Medio() {
+        
+    }
+
+    public void setLenguaje(Lenguaje lenguaje) {
+        this.lenguaje = lenguaje;
+    }
+}
---
+++ entregas/rivasMiguel/src/v2/Retroalimentacion.java
@@ -0,0 +1,7 @@
+public class Retroalimentacion {
+    private Mensaje mensajeInterno;
+
+    public Retroalimentacion() {
+        this.mensajeInterno = new Mensaje(); 
+    }
+}
