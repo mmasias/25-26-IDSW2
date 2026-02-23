27d6f4d98de0727b2fbe5aaa7c8e711062beef3b
Angel Romero <144820426+aeRomeroz@users.noreply.github.com>
2026-02-23 20:09:09 +0000

    feat(Plan): aladir constructor, getter y setters. Tambien añadir metódo para permitir ver un resumen del Objeto

--- entregas/romeroAngel/src/Plan.java
+++ entregas/romeroAngel/src/Plan.java
@@ -4,4 +4,39 @@ class Plan {
     private double price;
     private int maxProfiles;
     private boolean allowOffline;
+
+    public Plan(int id, String name, double price, int maxProfiles, boolean allowOffline) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.maxProfiles = maxProfiles;
+        this.allowOffline = allowOffline;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public int getMaxProfiles() {
+        return maxProfiles;
+    }
+
+    public boolean isAllowOffline() {
+        return allowOffline;
+    }
+
+    @Override
+    public String toString() {
+        return "=======" + "Plan: " + name + "=======" + "\n" + 
+                "Price: €" + price + "\n" +
+                "Max Profiles: " + maxProfiles + "\n" +
+                "Allow Offline: " + allowOffline + "\n" 
 }
