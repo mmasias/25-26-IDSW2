0c1307d5236c24085579eecb7fb6908dd74f62bb
Angel Romero <144820426+aeRomeroz@users.noreply.github.com>
2026-02-23 19:41:27 +0000

    feat(Song): añadir constructor, getters y setters

--- entregas/romeroAngel/src/Song.java
+++ entregas/romeroAngel/src/Song.java
@@ -7,4 +7,41 @@ class Song {
     private String fileUrl;
     private List<Artist> artists;
     private long playCount;
+
+    public Song(int id, String title, int duration, String fileUrl, List<Artist> artists) {
+        this.id = id;
+        this.title = title;
+        this.duration = duration;
+        this.fileUrl = fileUrl;
+        this.artists = artists;
+        this.playCount = 0;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public int getDuration() {
+        return duration;
+    }
+
+    public String getFileUrl() {
+        return fileUrl;
+    }
+
+    public List<Artist> getArtists() {
+        return artists;
+    }
+
+    public long getPlayCount() {
+        return playCount;
+    }
+
+    public void incrementPlayCount() {
+        playCount++;
+    }
 }
