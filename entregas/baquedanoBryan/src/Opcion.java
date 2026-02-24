b58dc27ec2a5a65ca3dfac47a14c14b68d2937eb
enmabry <bryanbaquedano11@gmail.com>
2026-02-24 23:36:37 +0200

    Agregar clase Opcion para gestionar descripciones

---
+++ entregas/baquedanoBryan/src/Opcion.java
@@ -0,0 +1,13 @@
+public class Opcion {
+
+    private String descripcion;
+
+    public Opcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+}
