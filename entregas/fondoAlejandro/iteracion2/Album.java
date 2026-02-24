0aa5109d7a555359b22335324b9e7b1b1399b19e
alejandrofondo <jose.fondo@alumnos.uneatlantico.es>
2026-02-24 19:11:07 +0100

    Modelado del ecosistema de la música actual - segunda iteración.

---
+++ entregas/fondoAlejandro/iteracion2/Album.java
@@ -0,0 +1,18 @@
+package iteracion2;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Album {
+    private String titulo;
+    private List<Cancion> canciones;
+
+    public Album(String titulo) {
+        this.titulo = titulo;
+        this.canciones = new ArrayList<>();
+    }
+
+    public void agregarCancion(Cancion c) {
+        canciones.add(c);
+    }
+}
