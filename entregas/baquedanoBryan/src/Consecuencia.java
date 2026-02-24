11112c20bcce0e0b94a1e41e8399479cbadab089
enmabry <bryanbaquedano11@gmail.com>
2026-02-24 23:39:10 +0200

    Agregar clases Consecuencia y Motivacion para gestionar efectos y causas

---
+++ entregas/baquedanoBryan/src/Consecuencia.java
@@ -0,0 +1,11 @@
+public class Consecuencia {
+    private String efecto;
+
+    public Consecuencia(String efecto) {
+        this.efecto = efecto;
+    }
+
+    public String getEfecto() {
+        return efecto;
+    }
+}
---
+++ entregas/baquedanoBryan/src/Motivacion.java
@@ -0,0 +1,11 @@
+public class Motivacion {
+    private String causa;
+
+    public Motivacion(String causa) {
+        this.causa = causa;
+    }
+
+    public String getCausa() {
+        return causa;
+    }
+}
