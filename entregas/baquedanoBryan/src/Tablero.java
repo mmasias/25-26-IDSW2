38247b13c11e1f2285e8fcde74de349129f9ad1c
enmabry <bryanbaquedano11@gmail.com>
2026-04-15 02:23:21 +0200

    Refactor la interfaz Tablero y agregar la implementación de TableroClasico

--- entregas/baquedanoBryan/src/Tablero.java
+++ entregas/baquedanoBryan/src/Tablero.java
@@ -1,91 +1,13 @@
-class Tablero {
+public interface Tablero {
 
-    private char[][] casillas;
-    final private int DIMENSION = 3;
-    final private char VACIO = '_';
-
-    public Tablero() {
-        casillas = new char[DIMENSION][DIMENSION];
-        for (int i = 0; i < DIMENSION; i++) {
-            for (int j = 0; j < DIMENSION; j++) {
-                casillas[i][j] = VACIO;
-            }
-        }
-        
-    }
-
-    public char getFicha(int fila, int columna) {
-        return this.casillas[fila][columna];
-
-    }
+    char getFicha(int fila, int columna);
+    boolean hayTresEnRaya();
+    boolean estaCompleto(Jugador jugador);
+    boolean estaOcupado(Coordenada coordenada);
+    boolean estaVacio(Coordenada coordenada);
+    boolean verificarColorFicha(Coordenada coordenada, char color);
 
 
-
-    public boolean hayTresEnRaya() {
-        return this.hayTresEnRaya('x') || this.hayTresEnRaya('o');
-    }
-
-    private boolean hayTresEnRaya(char color) {
-        int[] filas = new int[DIMENSION];
-        int[] columnas = new int[DIMENSION];
-        int diagonal = 0;
-        int secundaria = 0;
-
-        for (int i = 0; i < DIMENSION; i++) {
-            for (int j = 0; j < DIMENSION; j++) {
-                if (casillas[i][j] == color) {
-                    filas[i]++;
-                    columnas[j]++;
-                    diagonal = diagonal + ((i == j) ? 1 : 0);
-                    secundaria = secundaria + ((i + j == 2) ? 1 : 0);
-
-                    if (filas[i] == DIMENSION || columnas[j] == DIMENSION || diagonal == DIMENSION || secundaria == DIMENSION) {
-                        return true;
-                    }
-
-                }
-            }
-        }
-        return false;
-
-    }
-
-    public boolean estaCompleto(Jugador jugador) {
-        assert jugador != null;
-        int conteoFichas = 0;
-        for (int i = 0; i < DIMENSION; i++) {
-            for (int j = 0; j < DIMENSION; j++) {
-                if (casillas[i][j] == jugador.color()) {
-                    conteoFichas++;
-                }
-            }
-        }
-        return conteoFichas == 3;
-    }
-
-    public boolean estaOcupado(Coordenada coordenada) {
-        return !this.verificarColorFicha(coordenada, VACIO);
-    }
-
-    public void ponerFicha(Coordenada coordenada, char color) {
-        int fila = coordenada.getFila();
-        int columna = coordenada.getColumna();
-        casillas[fila][columna] = color;
-    }
-
-    public boolean estaVacio(Coordenada coordenada) {
-        return !this.estaOcupado(coordenada);
-    }
-
-    public boolean verificarColorFicha(Coordenada coordenada, char color) {
-        int fila = coordenada.getFila();
-        int columna = coordenada.getColumna();
-        return casillas[fila][columna] == color;
-    }
-
-    public void sacarFicha(Coordenada coordenada) {
-        int fila = coordenada.getFila();
-        int columna = coordenada.getColumna();
-        casillas[fila][columna] = VACIO;
-    }
+    void ponerFicha(Coordenada coordenada, char color);
+    void sacarFicha(Coordenada coordenada);
 }
